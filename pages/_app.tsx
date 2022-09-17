353217b7cb2147121bf9b026e54030662f9e4a32
vanDev <111028309+vanDev0@users.noreply.github.com>
2022-09-17 16:50:02 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -46,7 +46,7 @@ function App(props: AppProps) {
       <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=UA-187621071-2`} />
       <Script strategy="lazyOnload" >
           window?.dataLayer = window?.dataLayer || [];
-          function gtag(){dataLayer?.push(arguments);}
+          function gtag(){window?.dataLayer?.push(arguments);}
           gtag('js', new Date());
           gtag('config', 'UA-187621071-2', {
             page_path: window?.location?.pathname,
